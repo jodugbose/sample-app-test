9b759634cdac15ebb0c256aa16dcad2c165579a3
Oluwasogo <Oluwasogo.Odugbose>
2023-08-18 15:15:39 +0100

    Added UpperLowerCase class

---
+++ src/main/java/com/imterswitch/maven/UpperLowerCase.java
@@ -0,0 +1,21 @@
+package com.imterswitch.maven;
+
+public class UpperLowerCase {
+
+    public String wordToUpperCase(String word){
+        if (word != null && !word.isEmpty()){
+            String[] words = word.split(" ");
+            return words[0].toUpperCase();
+        }
+        throw new IllegalArgumentException("Word cannot be empty");
+    }
+
+    public String wordToLowerCase(String word){
+        if (word != null && !word.isEmpty()){
+            String[] words = word.split(" ");
+            return words[0].toLowerCase();
+        }
+        throw new IllegalArgumentException("Word cannot be empty");
+    }
+
+}
